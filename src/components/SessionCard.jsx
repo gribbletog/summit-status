d6db7229830803fa0e39d78dc75ddc5406db03d5
rnagy <rnagy@adobe.com>
2025-12-15 13:27:52 -0800

    Move Add WIP Data button inline with badges instead of below

--- src/components/SessionCard.jsx
+++ src/components/SessionCard.jsx
@@ -82,12 +82,12 @@ function SessionCard({ session, isWIP = false, hasWIPOverride = false, onEditWIP
           {session['SESSION STATUS'] && (
             <span className="badge badge-status">{session['SESSION STATUS']}</span>
           )}
+          {isWIP && onEditWIP && (
+            <button className="edit-wip-btn" onClick={onEditWIP} title="Edit WIP data">
+              {hasWIPOverride ? '📝 Edit WIP' : 'Add WIP Data'}
+            </button>
+          )}
         </div>
-        {isWIP && onEditWIP && (
-          <button className="edit-wip-btn" onClick={onEditWIP} title="Edit WIP data">
-            {hasWIPOverride ? '📝 Edit WIP' : 'Add WIP Data'}
-          </button>
-        )}
       </div>
 
       <div className="session-body">
